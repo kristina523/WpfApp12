3e2078859fa8d6fee7abd456c6a44866849d018e
Артём <@DESKTOP-1MI1908>
2024-04-17 01:32:53 +0300

    Добавьте файлы проекта.

---
+++ WpfApp12/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp12.BooksDataSetTableAdapters;
+
+namespace WpfApp12
+{
+    public partial class MainWindow : Window
+    {
+        BooksTableAdapter books = new BooksTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+         private void window(object sender, RoutedEventArgs e)
+        {
+            BooksGrid.Columns[0].Visibility = Visibility.Collapsed;
+            BooksGrid.Columns[2].Visibility = Visibility.Collapsed;
+        }
+    }
+}
